c92c6dcd8489e640e8b8fe1ecbd2be36dd6eaeb6
Jack Markley <jack.markley@gmail.com>
2014-04-14 22:59:52 +0200

    basic framework

---
+++ tests.py
@@ -0,0 +1,45 @@
+import unittest
+from pokerdeck import Deck, Card
+
+
+class CardCase(unittest.TestCase):
+
+    def testConstruct(self):
+        pass
+
+    def testEquality(self):
+        pass
+
+    def testValue(self):
+        pass
+
+    def testString(self):
+        pass
+
+
+class DeckCase(unittest.TestCase):
+
+    def testConstruct(self):
+        pass
+
+    def testCount(self):
+        pass
+
+    def testShuffle(self):
+        pass
+
+    def testCut(self):
+        pass
+
+    def testTopDeck(self):
+        pass
+
+    def testBottomDeck(self):
+        pass
+
+    def testDeal(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
