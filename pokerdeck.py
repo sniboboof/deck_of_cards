c94733262ebee0151cf321838ccb926a3b0b85aa
Jack Markley <jack.markley@gmail.com>
2014-04-16 20:51:32 +0200

    tests finished and class skeletons ready for the real code

--- pokerdeck.py
+++ pokerdeck.py
@@ -1,10 +1,64 @@
-
 import random
 
 
+#individual cards
+#a hand is basically a list of cards
+#a deck is basically a d.e.queue
 class Card():
-    pass
+    def __init__(self, value, suit):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __cmp__(self, operand):
+        pass
 
 
+#designed to behave like a real deck
+#it is kindof needlessly complex for a real game of cards
+#so the model for useful functions is based more on
+#card tricks and dirty dealing
+
+#think of bottom and front (0 index) of deck as the same (the face out side)
+#think of top and back (max index) of deck the same (face in side)
+
+#two functions have hidden booleans to change normal behavior
+#they can be used to covertly give people a known card
 class Deck():
-    pass
+    def __init__(self, cards):
+        pass
+
+    def __len__(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def sort(self):
+        pass
+
+    #reveal functions show a card without modifying the deck
+    #like a real deck, if you don't want information about the card's location
+    #you have to shuffle afterwards
+    def topDeck(self):
+        pass
+
+    def botDeck(self):
+        pass
+
+    def cut(self):
+        pass
+
+    #the only function that reduces deck size
+    #has a hidden overload
+    def deal(self, count=1, bottom=False):
+        pass
+
+    #the two ways to put cards back into the deck
+    #put has a hidden overload to put cards on top instead of where they belong
+    def put(self, cards, top=False):
+        pass
+
+    def shuffleIn(self, cards):
+        pass
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from pokerdeck import Deck, Card
+from copy import copy
 
 
 class CardCase(unittest.TestCase):
@@ -36,7 +37,10 @@ class CardCase(unittest.TestCase):
         self.assertEqual(specificCard.value, 13)
         self.assertEqual(specificCard.suit, 2)
 
-        for i, suitstring in enumerate(('club', 'Diamond', 'HEART', 'sPadE')):
+        for i, suitstring in enumerate(('clubs',
+                                        'Diamonds',
+                                        'HEARTs',
+                                        'sPadEs')):
             specificCard = Card(3, suitstring)
             self.assertEqual(specificCard.value, 3)
             self.assertEqual(specificCard.suit, i)
@@ -58,32 +62,124 @@ class CardCase(unittest.TestCase):
 
     def testString(self):
         assert "of" in str(Card())
-        self.assertEqual("Jack of Clubs", str(Card("jack", "club")))
-        self.assertEqual("4 of Diamonds", str(Card(4, "diamond")))
+        self.assertEqual("Jack of Clubs", str(Card("jack", "clubs")))
+        self.assertEqual("4 of Diamonds", str(Card(4, "diamonds")))
 
 
 class DeckCase(unittest.TestCase):
 
     def testConstruct(self):
-        pass
+        mydek = Deck()
+        assert mydek is not None
+        self.assertEqual(mydek.cards[0], Card(1, 0))
+        self.assertEqual(mydek.cards[51], Card(13, 3))
+        self.assertRaises(IndexError, mydek.cards.get(52))
+
+        smalldek = Deck([Card(1, 0)])
+        assert smalldek is not None
+        self.assertEqual(smalldek.cards[0], Card(1, 0))
+        self.assertRaises(IndexError, smalldek.cards.get(1))
+
+        meddek = Deck([Card(1, 0), Card('Queen', 'Hearts'), Card(1, 0)])
+        self.assertEqual(meddek.cards[0], meddek.cards[2])
+        self.assertEqual(meddek.cards[2], Card(1, 0))
+        self.assertEqual(meddek.cards[1], Card('Queen', 'Hearts'))
 
     def testCount(self):
-        pass
+        mydek = Deck()
+        self.assertEqual(len(mydek), 52)
 
-    def testShuffle(self):
-        pass
+        mydek = Deck([Card(1, 0), Card('Queen', 'hearts'), Card(1, 0)])
+        self.assertEqual(len(mydek), 3)
+
+    def testShuffleSort(self):
+        mydek = Deck()
+        imitation = copy(mydek)
+        mydek.shuffle()
+        self.assertNotEqual(mydek, imitation)
+        mydek.sort()
+        self.assertEqual(mydek, imitation)
 
     def testCut(self):
-        pass
+        mydek = Deck([Card('ace', 'clubs')])
+        imitation = copy(mydek)
+        self.assertEqual(mydek.cut(), 'Ace of Clubs')
+        self.assertEqual(mydek, imitation)
+
+        mydek = Deck()
+        imitation = copy(mydek)
+        assert 'of' in mydek.cut()
+        self.assertEqual(mydek, imitation)
 
     def testTopDeck(self):
-        pass
+        mydek = Deck([Card('Queen', 'Hearts')])
+        imitation = copy(mydek)
+        self.assertEqual(mydek.topDeck(), 'Queen of Hearts')
+        self.assertEqual(mydek, imitation)
+
+        mydek = Deck()
+        imitation = mydek
+        self.assertEqual(mydek.topDeck(), 'King of Spades')
+        self.assertEqual(mydek, imitation)
 
     def testBottomDeck(self):
-        pass
+        mydek = Deck([Card('Queen', 'Hearts')])
+        imitation = copy(mydek)
+        self.assertEqual(mydek.botDeck(), 'Queen of Hearts')
+        self.assertEqual(mydek, imitation)
+
+        mydek = Deck()
+        imitation = mydek
+        self.assertEqual(mydek.botDeck(), 'Ace of Clubs')
+        self.assertEqual(mydek, imitation)
 
     def testDeal(self):
-        pass
+        mydek = Deck([])
+        self.assertRaises(IndexError, mydek.deal())
+
+        mydek = Deck([Card('queen', 'hearts')])
+        myhand = []
+        self.assertRaises(IndexError, mydek.deal(2))
+        myhand.append(mydek.deal())
+        self.assertEqual(myhand[0], Card('queen', 'hearts'))
+        assert myhand[0] not in mydek.cards
+        self.assertEqual(len(myhand), 51)
+
+        mydek = Deck()
+        myhand = []
+        myhand.append(mydek.deal(2))
+        self.assertEqual(myhand, [Card('king', 'spades'),
+                                  Card('queen', 'spades')])
+
+        #if they find out this overload exists, you could get shot
+        mydek = Deck()
+        myhand = []
+        myhand.append(mydek.deal(2, True))
+        self.assertEqual(myhand, [Card('ace', 'clubs'),
+                                  Card('2', 'clubs')])
+
+    def testPutTopBot(self):
+        start = [Card('queen', 'hearts')]
+        mydek = Deck(copy(start))
+
+        toTop = [Card('ace', 'spades'), Card('king', 'hearts')]
+        mydek.put(copy(toTop), True)
+
+        toBot = [Card('1', 'clubs'), Card('2', 'clubs')]
+        mydek.put(copy(toBot))
+
+        self.assertEqual(len(mydek), 5)
+        self.assertEqual(mydek.cards, toBot+start+toTop)
+
+    def testShuffleIn(self):
+        mydek = Deck()
+        imitation = copy(mydek)
+        mydek.deal(2)
+        mydek.shuffleIn([Card('king', 'spades'),
+                         Card('queen', 'spades')])
+        self.assertNotEqual(mydek, imitation)
+        mydek.sort()
+        self.assertEqual(mydek, imitation)
 
 
 if __name__ == "__main__":
